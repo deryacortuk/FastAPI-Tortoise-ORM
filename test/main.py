704688bd029ee73827db99d0db7fa289f0064346
Derya Cortuk <53267226+deryacortuk@users.noreply.github.com>
2023-03-12 21:02:56 +0300

    Delete test directory

--- test/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI,Form
-
-app =FastAPI()
-
-@app.post("/test/")
-async def testFast(name:str= Form(...),type:str=Form(...)):
-    return {"python":name,"fastapi":type}
-    
